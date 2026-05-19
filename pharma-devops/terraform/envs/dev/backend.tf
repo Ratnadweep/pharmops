a86e4c24d07c5f49cdd43a00ad04fc16b69eba54
Ratnadweep <rghosh03311995@gmail.com>
2026-05-19 13:41:22 +0530

    updating pharma-devops/terraform/envs/dev/backend.tf and workflows

--- pharma-devops/terraform/envs/dev/backend.tf
+++ pharma-devops/terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pharma-tf-state-873135413040"
+    bucket         = "pharma-tf-state-449901517774"
     key            = "envs/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
